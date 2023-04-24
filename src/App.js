9c826c334e3b550a38feb0d193e78803e6b2299d
Théo Bourdier <theobourdier1@gmail.com>
2023-04-24 23:20:54 +0200

    Settings & Profile tab

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import "./index.css";
 import Navbar from "./components/Navbar";
 
 import Home from "./routes/Home";
+import Profile from "./routes/Profile";
+import Settings from "./routes/Settings";
 
 export default function App() {
   return (
@@ -13,6 +15,8 @@ export default function App() {
       <div className="View">
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/profile" element={<Profile/>} />
+        <Route path="/settings" element={<Settings/>} />
       </Routes>
       </div>
       <Navbar/>
---
+++ src/routes/Settings.js
@@ -0,0 +1,18 @@
+import Card from "../components/Card";
+import Upperbar from "../components/Upperbar";
+
+export default function Home() {
+  const user = "User";
+  return (
+    <>
+      <Upperbar>
+        <ul>
+          <li>Paramètres</li>
+        </ul>
+      </Upperbar>
+      <div className="Profile">
+
+      </div>
+    </>
+  )
+}
