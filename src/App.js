cab6fe30e39ec3ff2b69c8a5d45209e6e7d8d931
Théo Bourdier <theobourdier1@gmail.com>
2023-04-24 21:33:07 +0200

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import "./index.css";
+
 export default function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+      </Routes>
     </div>
   );
 }
