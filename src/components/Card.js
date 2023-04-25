8d601844ba40c71a9cb52ee4749315714c52f43a
Théo Bourdier <theobourdier1@gmail.com>
2023-04-25 15:44:49 +0200

    Stats

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,7 +10,7 @@ export default function Card(tmdb) {
   const randomPage = randomInterval(1, 15);
 
   useEffect(() => {
-    fetch('https://api.themoviedb.org/3/discover/movie?api_key='+ tmdb.api + '&language=fr-FR&sort_by=popularity.desc&include_adult=false&include_video=false&page=' + randomPage + '&year=2023&with_watch_monetization_types=flatrate')
+    fetch('https://api.themoviedb.org/3/discover/movie?api_key='+ tmdb.api + '&language=fr-FR&sort_by=popularity.desc&include_adult=false&include_video=false&page=' + randomPage + '&year=2023&vote_count.gte=150&vote_average.gte=5&with_watch_monetization_types=flatrate')
       .then(res => res.json())
       .then(
         (result) => {
@@ -29,7 +29,9 @@ export default function Card(tmdb) {
         <div className="card__infos">
           <span className="card__infos__title">{movie.title}</span>
           <ul className="card__tags">
-            <li>{movie.release_date}</li>
+            <li><i class="fa-solid fa-calendar-days"></i> {movie.release_date}</li>
+            <li><i class="fa-regular fa-star"></i> {movie.vote_average}</li>
+            <li><i class="fa-solid fa-users"></i> {movie.vote_count}</li>
           </ul>
         </div>
         <img src={"https://www.themoviedb.org/t/p/w600_and_h900_bestv2" + movie.poster_path} alt="..." />
