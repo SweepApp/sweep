d08d473ee9a2c066be5bbb119dd2375104844080
Théo Bourdier <theobourdier1@gmail.com>
2023-04-25 13:31:34 +0200

    Pass TMDB API KEY as props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import "./index.css";
 
-// import Upperbar from "./components/Upperbar";
 import Navbar from "./components/Navbar";
 
 import Home from "./routes/Home";
@@ -9,18 +8,16 @@ import Profile from "./routes/Profile";
 import Swipe from "./routes/Swipe";
 
 export default function App() {
-  const user = "User";
+  const username = "User";
   const tmdbApiKey = process.env.REACT_APP_TMDB_API;
-  console.log(tmdbApiKey);
 
   return (
     <div className="App">
-      {/* <Upperbar/> */}
       <div className="View">
       <Routes>
-        <Route path="/" element={<Home user={user}/>} />
-        <Route path="/profile" element={<Profile user={user}/>} />
-        <Route path="/swipe" element={<Swipe user={user}/>} />
+        <Route path="/" element={<Home user={username} tmdb={tmdbApiKey}/>} />
+        <Route path="/profile" element={<Profile user={username} tmdb={tmdbApiKey}/>} />
+        <Route path="/swipe" element={<Swipe user={username} tmdb={tmdbApiKey}/>} />
       </Routes>
       </div>
       <Navbar/>
