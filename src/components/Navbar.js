074a363706d9fe1eaff2b9ef59e484012681790e
Théo Bourdier <theobourdier1@gmail.com>
2023-04-25 13:35:24 +0200

    Fix 'class' react error

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,17 +10,17 @@ export default function Navbar() {
       <ul>
         <li className={splitLocation[1] === "swipe" ? "active" : ""}>
           <a href="/swipe">
-            <i class="fa-solid fa-children fa-fw"></i>
+            <i className="fa-solid fa-children fa-fw"></i>
           </a>
         </li>
         <li className={splitLocation[1] === "" ? "active" : ""}>
           <a href="/">
-            <i class="fa-solid fa-house fa-fw"></i>
+            <i className="fa-solid fa-house fa-fw"></i>
           </a>
         </li>
         <li className={splitLocation[1] === "profile" ? "active" : ""}>
           <a href="/profile">
-            <i class="fa-solid fa-user fa-fw"></i>
+            <i className="fa-solid fa-user fa-fw"></i>
           </a>
         </li>
       </ul>
