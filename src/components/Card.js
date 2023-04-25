fd105a2cbf9880e2ad2e06ec25316858cc31e685
Théo Bourdier <theobourdier1@gmail.com>
2023-04-25 15:30:55 +0200

    Display random movies

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,16 @@
 import {useState, useEffect} from 'react';
 
-import example from "../assets/images/starwars.jpg";
-
 export default function Card(tmdb) {
   const [api, setApi] = useState([]);
 
-  function randomInt(min, max) {
+  function randomInterval(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
-  const rdnInt = randomInt(1,500);
+  const rdnInt = randomInterval(1, 15);
 
   useEffect(() => {
-    fetch('https://api.themoviedb.org/3/movie/' + rdnInt + '?api_key='+ tmdb.api)
+    fetch('https://api.themoviedb.org/3/discover/movie?api_key='+ tmdb.api + '&language=fr-FR&sort_by=popularity.desc&include_adult=false&include_video=false&page=' + rdnInt + '&year=2023&with_watch_monetization_types=flatrate')
       .then(res => res.json())
       .then(
         (result) => {
@@ -25,16 +23,20 @@ export default function Card(tmdb) {
   console.log(api)
 
   return (
-    api && (
+    api.results !== undefined ? (
+      api.results.map((movie) => 
       <div className="card">
         <div className="card__infos">
-          <span className="card__infos__title">{api.original_title}</span>
+          <span className="card__infos__title">{movie.title}</span>
           <ul className="card__tags">
-            <li>Test</li>
+            <li>{movie.release_date}</li>
           </ul>
         </div>
-        <img src={"https://www.themoviedb.org/t/p/w600_and_h900_bestv2" + api.poster_path} alt="..." />
+        <img src={"https://www.themoviedb.org/t/p/w600_and_h900_bestv2" + movie.poster_path} alt="..." />
       </div>
+      )
+    ) : (
+      <p>Loading</p>
     )
-  );
+  )
 }
