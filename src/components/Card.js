f5bbdd98c9688ce570e55835af2f87a7ea196db8
Théo Bourdier <theobourdier1@gmail.com>
2023-04-25 14:15:12 +0200

    Display random movie

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,40 @@
-import img from "../assets/images/starwars.jpg";
+import {useState, useEffect} from 'react';
+
+import example from "../assets/images/starwars.jpg";
 
 export default function Card(tmdb) {
+  const [api, setApi] = useState([]);
+
+  function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  const rdnInt = randomInt(1,500);
+
+  useEffect(() => {
+    fetch('https://api.themoviedb.org/3/movie/' + rdnInt + '?api_key='+ tmdb.api)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setApi(result);
+        }
+      )
+  }, []
+  ) 
+
+  console.log(api)
+
   return (
-    <div className="card">
-      <div className="card__infos">
-        <span className="card__infos__title">The Mandalorian</span>
-        <ul className="card__tags">
-          <li>Science-Fiction</li>
-          <li>Western</li>
-        </ul>
+    api && (
+      <div className="card">
+        <div className="card__infos">
+          <span className="card__infos__title">{api.original_title}</span>
+          <ul className="card__tags">
+            <li>Test</li>
+          </ul>
+        </div>
+        <img src={"https://www.themoviedb.org/t/p/w600_and_h900_bestv2" + api.poster_path} alt="..." />
       </div>
-      <img src={img} alt="..." />
-    </div>
+    )
   );
 }
