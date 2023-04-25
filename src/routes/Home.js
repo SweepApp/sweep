78386decbe7df449f4bdd42c8abcec97a7959332
Théo Bourdier <theobourdier1@gmail.com>
2023-04-25 11:15:44 +0200

    Remove friends

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,17 +2,18 @@ import Card from "../components/Card";
 import Upperbar from "../components/Upperbar";
 
 export default function Home() {
-  const user = "User";
+  const user = "Théo";
+
+
   return (
     <>
       <Upperbar>
         <ul>
           <li><button>Populaire</button></li>
-          <li><button>Pour vous</button></li>
-          <li><button>Vos amis</button></li>
+          {/* <li><button>Vos amis</button></li> */}
         </ul>
       </Upperbar>
-      <div className="Home">
+      <div className="Home" id="popular">
         <h1>Bonjour, { user } 👋</h1>
         <div className="Home__cards">
           <Card />
