68f7489da1d110259df52bb13a1fe7441fd55530
Théo Bourdier <theobourdier1@gmail.com>
2023-06-15 14:42:48 +0200

    Add Backdrop_path poster

--- src/redux/movieDetails/reducer.jsx
+++ src/redux/movieDetails/reducer.jsx
@@ -6,6 +6,7 @@ const movieDetailsInitialState = {
   release_date: null,
   runtime: null,
   poster_path: null,
+  backdrop_path: null,
   overview: null,
   genres: [],
   production_companies: [],
@@ -21,12 +22,13 @@ export const movieDetailsSlice = createSlice({
   initialState: movieDetailsInitialState,
   reducers: {
     setMovieDetails: (state, action) => {
-      const { title, tagline, release_date, runtime, poster_path, overview, genres, production_companies, credits, revenue, budget, keywords, recommendations } = action.payload;
+      const { title, tagline, release_date, runtime, poster_path, backdrop_path, overview, genres, production_companies, credits, revenue, budget, keywords, recommendations } = action.payload;
       state.title = title;
       state.tagline = tagline;
       state.release_date = release_date;
       state.runtime = runtime;
       state.poster_path = poster_path;
+      state.backdrop_path = backdrop_path;
       state.overview = overview;
       state.genres = Object.values(genres[0]).slice(0, -1).map(value => value.split('-')).join('').split(',');
       state.production_companies = Object.values(production_companies[0]).slice(0, -1).map(value => value.split('-')).join('').split(',');
