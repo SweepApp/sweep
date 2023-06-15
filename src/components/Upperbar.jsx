49e44a62abf667f9bfd926ae6e574db9357574e9
Théo Bourdier <theobourdier1@gmail.com>
2023-06-15 15:26:34 +0200

    Add back button on details

--- src/components/Upperbar.jsx
+++ src/components/Upperbar.jsx
@@ -1,7 +1,18 @@
+import { useLocation } from "react-router-dom";
+
 export default function Upperbar(props) {
+  const location = useLocation();
+  const { pathname } = location;
+  const splitLocation = pathname.split("/");
+
   return (
     <div id="upperbar">
+      {splitLocation[1] === "details" && (
+        <button className="no" onClick={() => window.history.back()}>
+          <i className="fa-solid fa-arrow-left"></i>
+        </button>
+      )}
       <h2>{props.title}</h2>
     </div>
-  )
+  );
 }
