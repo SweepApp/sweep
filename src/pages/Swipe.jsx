4a86adabf81a09d31ffacdfa945b1feca102a866
Théo Bourdier <theobourdier1@gmail.com>
2023-06-15 13:58:09 +0200

    Rework Party page

--- src/pages/Swipe.jsx
+++ src/pages/Swipe.jsx
@@ -1,19 +1,13 @@
 import { useNavigate } from "react-router";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
-import FriendCard from "../components/Friend";
 import Upperbar from "../components/Upperbar";
 
 export default function Home() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const token = useSelector((state) => state.usersData.token);
-  // const friendSelector = useSelector((state) => state.friendsData.friend);
-  const [modalState, setModalState] = useState(false);
-  const [friendsUsername, setFriendsUsername] = useState("");
-  const [friendButton, setFriendButton] = useState(true);
-
-  let friends = 1;
+  const [partyCode, setPartyCode] = useState("");
 
   useEffect(() => {
     if (!token) {
@@ -29,35 +23,22 @@ export default function Home() {
     <>
       <Upperbar title="Duo" />
       <div className="duo">
-        <div className="float__bottom">
-          {friendButton && (
-            <button className="wide">
-              Start a party
-            </button>
-          )}
-        </div>
-
         <div className="duo__content">
-
-          {modalState && (
-            <div className="modal">
-              <div className="modal__header">
-                <h2>Add friends</h2>
-                <button className="close no">
-                  <i className="fa-solid fa-fw fa-times"></i>
-                </button>
-              </div>
-              <form className="modal__body" onSubmit={handleFormSubmit}>
-                <input
-                  type="text"
-                  placeholder="@user"
-                  onChange={(e) => setFriendsUsername(e.target.value)}
-                  required
-                />
-                <button className="wide">Add</button>
-              </form>
-            </div>
-          )}
+          <div className="duo__content__header">
+            <h2>Party</h2>
+          </div>
+          <form className="modal__body" onSubmit={handleFormSubmit}>
+            <input
+              type="text"
+              placeholder="Party ID"
+              onChange={(e) => setPartyCode(e.target.value)}
+              required
+            />
+            <button className="sign">Join</button>
+          </form>
+        </div>
+        <div className="float__bottom">
+          <button className="wide">Start a party</button>
         </div>
       </div>
     </>
