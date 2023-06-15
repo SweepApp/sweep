df6ae651be1c425692ec8b513b39daded50fcede
Théo Bourdier <theobourdier1@gmail.com>
2023-06-15 14:22:00 +0200

    Party input style

--- src/pages/Party.jsx
+++ src/pages/Party.jsx
@@ -26,20 +26,23 @@ export default function Home() {
       <div className="party">
         <div className="party__content">
           <div className="party__content__header">
-            <Banner 
+            <Banner
               image="https://images.unsplash.com/photo-1598899134739-24c46f58b8c0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1156&q=80"
               alt="Party"
             />
+            <h2>Join a Party</h2>
           </div>
-          <form className="modal__body" onSubmit={handleFormSubmit}>
-            <input
-              type="text"
-              placeholder="Party ID"
-              onChange={(e) => setPartyCode(e.target.value)}
-              required
-            />
-            <button className="sign">Join</button>
-          </form>
+          <div className="party__content__body">
+            <form onSubmit={handleFormSubmit}>
+              <input
+                type="text"
+                placeholder="Party ID"
+                onChange={(e) => setPartyCode(e.target.value)}
+                required
+              />
+              <button className="sign">Join</button>
+            </form>
+          </div>
         </div>
         <div className="float__bottom">
           <button className="wide">Start a party</button>
